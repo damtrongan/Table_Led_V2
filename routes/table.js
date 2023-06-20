83e0e93758cb2c29de14263a3c1561ccda31cc51
damtrongan <damtrongan@gmail.com>
2023-06-21 00:42:36 +0700

    test read file callback

--- routes/table.js
+++ routes/table.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const fs = require("fs");
-const { rejects } = require("assert");
 const configs = require("../data/config").configs;
 
 const main_Folder = configs.dir_readFile;
@@ -12,6 +11,7 @@ const nameFolderTable2s = configs.names_folder_table2;
 const pathFolderRead = nameFolderTable1s.map((nameFolder) => {
   return `${main_Folder}/${nameFolder}`;
 });
+
 //console.log(pathFolderRead);
 /* 
   IN:
@@ -22,34 +22,76 @@ const pathFolderRead = nameFolderTable1s.map((nameFolder) => {
   + Đọc ra file mới nhất trong đó
 */
 
-function getNewestFile(pathReadFoler) {
-  return new Promise((resolve, rejects) => {
-    fs.readdir(pathReadFoler, {withFileTypes : true}, (err, files) => {
-      pathRead = '';
-      if (err) rejects(err);
-      else {
-        files = files.sort((a, b) => {
-          return b.name - a.name;
-        });
-        if(files[0].isDirectory){
-          getNewestFile(`${pathReadFoler}/${files[0].name}`)
-        }else{
-          return ( pathRead =`${pathReadFoler}/${files[0].name}`);
-        }
-        resolve(pathRead);
-      }
-    });
-  });
+// function getNewestFile(pathReadFoler) {
+//   return new Promise((resolve, rejects) => {
+//     fs.readdir(pathReadFoler, {withFileTypes : true}, (err, files) => {
+//       pathRead = '';
+//       if (err) rejects(err);
+//       else {
+//         files = files.sort((a, b) => {
+//           return b.name - a.name;
+//         });
+//         if(files[0].isDirectory){
+//           getNewestFile(`${pathReadFoler}/${files[0].name}`)
+//         }else{
+//           return ( pathRead =`${pathReadFoler}/${files[0].name}`);
+//         }
+//         resolve(pathRead);
+//       }
+//     });
+//   });
+// }
+
+// getNewestFile(pathFolderRead[0])
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// var p = "D:/02_Code/03_Table_Led_V2/data/test.json";
+// var a = true;
+// function readFileJSON(p){
+//   fs.readFile(p,'utf8', (err, fileContent) => {
+//     if(err) {
+//       fileContent = [];
+//       readFileJSON(fileContent);
+//     }else {
+//       console.log(fileContent);
+//     }
+//   })
+// };
+// readFileJSON(p);
+
+
+
+// const getProductsFromFile = (cb) => {
+//   fs.readFile(p, (err, fileContent) => {
+//     if (err) {
+//       cb([]);
+//     } else {
+//       cb(JSON.parse(fileContent));
+//     }
+//   });
+// };
+// var AN = { name: "an" , age : 17};
+
+// getProductsFromFile(test => {
+//   console.log(test);
+// });
+// getProductsFromFile(AN);
+
+var product1 = {
+  name : "an",
+  age : "123",
 }
-
-
-getNewestFile(pathFolderRead[0])
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+var product2 = {
+  name : "trang",
+  age : "123",
+}
+var products = [product1, product2];
+console.log([...products]);
 
 function renderPage(req, res, next) {
   res.render("../views/table.ejs", {
