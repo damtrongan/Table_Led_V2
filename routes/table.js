16ca03870162f306082f60aae2967c5c89548035
TRONG AN_Dam <trongan.dam@vn.panasonic.com>
2023-07-26 21:36:46 +0700

    aa

--- routes/table.js
+++ routes/table.js
@@ -1,89 +1,44 @@
 const express = require("express");
 const router = express.Router();
+
 const path = require("path");
 const fs = require("fs");
-const configs = require("../data/config").configs;
 
-const main_Folder = configs.dir_readFile;
-const nameFolderTable1s = configs.names_folder_table1;
-const nameFolderTable2s = configs.names_folder_table2;
-
-const pathFolderRead = nameFolderTable1s.map((nameFolder) => {
-  return path.join(main_Folder, nameFolder);
+const configs = require("../data/config.json");
+const mainPath = configs.dir_readFile;
+const pathTable1 = configs.names_folder_table1.map((nameFolder) => {
+  return `${mainPath}/${nameFolder}`;
+});
+const pathTable2 = configs.names_folder_table2.map((nameFolder) => {
+  return `${mainPath}/${nameFolder}`;
 });
 
-
-//console.log(pathFolderRead);
-/* 
-  IN:
-  + main_folder cần đọc
-  + Mảng tên folder cần đọc
-
-  OUT: 
-  + Đọc ra file mới nhất trong đó
-*/
-
-// function getNewestFile(pathReadFoler) {
-//   return new Promise((resolve, rejects) => {
-//     fs.readdir(pathReadFoler, {withFileTypes : true}, (err, files) => {
-//       pathRead = '';
-//       if (err) rejects(err);
-//       else {
-//         files = files.sort((a, b) => {
-//           return b.name - a.name;
-//         });
-//         if(files[0].isDirectory){
-//           getNewestFile(`${pathReadFoler}/${files[0].name}`)
-//         }else{
-//           return ( pathRead =`${pathReadFoler}/${files[0].name}`);
-//         }
-//         resolve(pathRead);
-//       }
-//     });
-//   });
-// }
-
-// getNewestFile(pathFolderRead[0])
-//   .then((result) => {
-//     console.log(result);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// var p = "D:/02_Code/03_Table_Led_V2/data/test.json";
-// var a = true;
-// function readFileJSON(p){
-//   fs.readFile(p,'utf8', (err, fileContent) => {
-//     if(err) {
-//       fileContent = [];
-//       readFileJSON(fileContent);
-//     }else {
-//       console.log(fileContent);
-//     }
-//   })
-// };
-// readFileJSON(p);
-
-
-var p = "./data/xxx.json/"
-const getProductsFromFile = (cb) => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
+function getIntoFolder(path) {
+  const promise = new Promise((resolve, reject) => {
+    fs.readdir(path, { withFileTypes: true }, (err, folders) => {
+      if (err) {
+        reject("Can not read folder");
+      }else{
+        resolve(folders);
+      }
+      // let newestFolder = folders[folders.length - 1];
+      // if (newestFolder.isDirectory) {
+      //   getIntoFolder(`${path}/${newestFolder.name}`);
+      // } else {
+      //   resolve(`${path}/${newestFolder.name}`);
+      // }
+    });
   });
-};
-
-function test(){
-  getProductsFromFile((data) => {
-    console.log(data);
-  })
+  return promise;
 }
-
-test();
+console.log(pathTable1);
+getIntoFolder(pathTable1[0])
+  .then((path) => {
+    console.log(path);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 function renderPage(req, res, next) {
   res.render("../views/table.ejs", {
