dc7b16cac5380e675f5d5127546d5a5e5bd01f91
damtrongan <damtrongan@gmail.com>
2023-07-26 23:56:29 +0700

    not done with read file

--- routes/table.js
+++ routes/table.js
@@ -6,6 +6,7 @@ const fs = require("fs");
 
 const configs = require("../data/config.json");
 const mainPath = configs.dir_readFile;
+
 const pathTable1 = configs.names_folder_table1.map((nameFolder) => {
   return `${mainPath}/${nameFolder}`;
 });
@@ -13,28 +14,27 @@ const pathTable2 = configs.names_folder_table2.map((nameFolder) => {
   return `${mainPath}/${nameFolder}`;
 });
 
-function getIntoFolder(path) {
-  const promise = new Promise((resolve, reject) => {
+function readDir(path) {
+  return new Promise((resolve, rejects) => {
     fs.readdir(path, { withFileTypes: true }, (err, folders) => {
       if (err) {
-        reject("Can not read folder");
-      }else{
-        resolve(folders);
+        console.log(err);
+      } else {
+        var newestFolder = folders[folders.length - 1];
+        console.log(newestFolder.isDirectory());
+        if (newestFolder.isDirectory && newestFolder.name.endsWith('.txt') ) {
+          readDir(`${path}/${newestFolder.name}`);
+        } else {
+          resolve(`${path}/${newestFolder.name}`);
+        }
       }
-      // let newestFolder = folders[folders.length - 1];
-      // if (newestFolder.isDirectory) {
-      //   getIntoFolder(`${path}/${newestFolder.name}`);
-      // } else {
-      //   resolve(`${path}/${newestFolder.name}`);
-      // }
     });
   });
-  return promise;
 }
-console.log(pathTable1);
-getIntoFolder(pathTable1[0])
-  .then((path) => {
-    console.log(path);
+
+readDir(pathTable1[0])
+  .then((data) => {
+    console.log(data);
   })
   .catch((err) => {
     console.log(err);
