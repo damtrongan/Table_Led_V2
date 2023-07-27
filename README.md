562f17c3b59819ca64d1e77f3186b61ac2c7fc00
damtrongan <damtrongan@gmail.com>
2023-07-27 23:48:57 +0700

    write function get newest file

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # Table_Led
 
 # Version 1.0.0
-
 This version can read file text from local path.
 
 # Version 2.0.0
 New feature can read data from FTP Server
+
 # Table_Led_V2
 This is Table Led NodeJS Project Version 2
-
-# Test push
--- routes/table.js
+++ routes/table.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const path = require("path");
-const fs = require("fs");
-
+const fs = require("fs").promises;
 const configs = require("../data/config.json");
+
 const mainPath = configs.dir_readFile;
 
 const pathTable1 = configs.names_folder_table1.map((nameFolder) => {
@@ -14,31 +14,56 @@ const pathTable2 = configs.names_folder_table2.map((nameFolder) => {
   return `${mainPath}/${nameFolder}`;
 });
 
-function readDir(path) {
-  return new Promise((resolve, rejects) => {
-    fs.readdir(path, { withFileTypes: true }, (err, folders) => {
-      if (err) {
-        console.log(err);
-      } else {
-        var newestFolder = folders[folders.length - 1];
-        console.log(newestFolder.isDirectory());
-        if (newestFolder.isDirectory && newestFolder.name.endsWith('.txt') ) {
-          readDir(`${path}/${newestFolder.name}`);
-        } else {
-          resolve(`${path}/${newestFolder.name}`);
-        }
-      }
-    });
-  });
+async function getNewestPath(path) {
+  let folders = await fs.readdir(path);
+  let folder = folders[folders.length - 1];
+  return `${path}/${folder}`;
 }
 
-readDir(pathTable1[0])
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
+async function readNewestFilePath(paths) {
+  let contents = [];
+  for (const path of paths) {
+    const pathYear = await getNewestPath(path);
+    const pathMonth = await getNewestPath(pathYear);
+    const pathDay = await getNewestPath(pathMonth);
+    const pathFile = await getNewestPath(pathDay);
+    const data = await fs.readFile(pathFile, "utf8");
+    contents.push(data);
+  }
+  return contents;
+}
+
+const example = [
+  'pH\t2.34\t\t20230509153700\t00\r\n' +
+    'TSS\t65.61\tmg/L\t20230509153700\t00\r\n' +
+    'COD\t75.68\tmg/L\t20230509153700\t00\r\n' +
+    'Flow\t6.24\tm3/h\t20230509153700\t00\r\n' +
+    'NH4\t8.21\tmg/L\t20230509153700\t00\r\n' +
+    'Flow_IN\t298.29\tm3/h\t20230509153700\t00\r\n' +
+    'Temp\t22.87\toC\t20230509153700\t00\r\n',
+  'pH\t7.33\t\t20230509153800\t00\r\n' +
+    'TSS\t6.93\tmg/L\t20230509153800\t00\r\n' +
+    'COD\t62.98\tmg/L\t20230509153800\t00\r\n' +
+    'Flow\t40.28\tm3/h\t20230509153800\t00\r\n' +
+    'NH4\t6.46\tmg/L\t20230509153800\t00\r\n' +
+    'Flow_IN\t64.98\tm3/h\t20230509153800\t00\r\n' +
+    'Temp\t23.58\toC\t20230509153800\t00\r\n',
+  'pH\t7.71\t\t20230509154000\t00\r\n' +
+    'TSS\t30.92\tmg/L\t20230509154000\t00\r\n' +
+    'COD\t31.53\tmg/L\t20230509154000\t00\r\n' +
+    'Flow\t1.10\tm3/h\t20230509154000\t00\r\n' +
+    'NH4\t1.65\tmg/L\t20230509154000\t00\r\n' +
+    'Flow_IN\t0.00\tm3/h\t20230509154000\t02\r\n' +
+    'Temp\t28.93\toC\t20230509154000\t00\r\n'
+];
+
+function splitData(contents){
+  contents.forEach(element => {
+    element.split('\t')
+    console.log(element);
   });
+}
+splitData(example)
 
 function renderPage(req, res, next) {
   res.render("../views/table.ejs", {
