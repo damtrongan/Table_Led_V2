e567a2f4e5ff7435f9c1817a5c3027a137bcc341
TRONG AN_Dam <trongan.dam@vn.panasonic.com>
2023-07-28 16:56:02 +0700

    aa

--- routes/table.js
+++ routes/table.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const path = require("path");
 const fs = require("fs").promises;
+// const fs = require("fs");
+// const readline = require('readline');
 const configs = require("../data/config.json");
 
 const mainPath = configs.dir_readFile;
@@ -21,49 +23,67 @@ async function getNewestPath(path) {
 }
 
 async function readNewestFilePath(paths) {
-  let contents = [];
+  let contents = {};
+  
   for (const path of paths) {
     const pathYear = await getNewestPath(path);
     const pathMonth = await getNewestPath(pathYear);
     const pathDay = await getNewestPath(pathMonth);
     const pathFile = await getNewestPath(pathDay);
     const data = await fs.readFile(pathFile, "utf8");
-    contents.push(data);
+    const spliceData = data.split("\n").forEach((line) => {
+      let parameterOut = {};
+      var slicesArray = line.split("\t");
+      parameterOut[slicesArray[0]] = {
+        value: slicesArray[1],
+        unit: slicesArray[2],
+        time: slicesArray[3],
+        status: slicesArray[4],
+      };
+      return parameterOut
+    });
+    console.log(spliceData);
+    // const stationName = slices[slices.length-1];
+    //contents[stationName] = spliceData;
   }
   return contents;
 }
 
-const example = [
-  'pH\t2.34\t\t20230509153700\t00\r\n' +
-    'TSS\t65.61\tmg/L\t20230509153700\t00\r\n' +
-    'COD\t75.68\tmg/L\t20230509153700\t00\r\n' +
-    'Flow\t6.24\tm3/h\t20230509153700\t00\r\n' +
-    'NH4\t8.21\tmg/L\t20230509153700\t00\r\n' +
-    'Flow_IN\t298.29\tm3/h\t20230509153700\t00\r\n' +
-    'Temp\t22.87\toC\t20230509153700\t00\r\n',
-  'pH\t7.33\t\t20230509153800\t00\r\n' +
-    'TSS\t6.93\tmg/L\t20230509153800\t00\r\n' +
-    'COD\t62.98\tmg/L\t20230509153800\t00\r\n' +
-    'Flow\t40.28\tm3/h\t20230509153800\t00\r\n' +
-    'NH4\t6.46\tmg/L\t20230509153800\t00\r\n' +
-    'Flow_IN\t64.98\tm3/h\t20230509153800\t00\r\n' +
-    'Temp\t23.58\toC\t20230509153800\t00\r\n',
-  'pH\t7.71\t\t20230509154000\t00\r\n' +
-    'TSS\t30.92\tmg/L\t20230509154000\t00\r\n' +
-    'COD\t31.53\tmg/L\t20230509154000\t00\r\n' +
-    'Flow\t1.10\tm3/h\t20230509154000\t00\r\n' +
-    'NH4\t1.65\tmg/L\t20230509154000\t00\r\n' +
-    'Flow_IN\t0.00\tm3/h\t20230509154000\t02\r\n' +
-    'Temp\t28.93\toC\t20230509154000\t00\r\n'
-];
+readNewestFilePath(pathTable1).then((contents) => {
+  console.log(contents);
+});
 
-function splitData(contents){
-  contents.forEach(element => {
-    element.split('\t')
-    console.log(element);
-  });
-}
-splitData(example)
+// const example = [
+//   'pH\t2.34\t\t20230509153700\t00\r\n' +
+//     'TSS\t65.61\tmg/L\t20230509153700\t00\r\n' +
+//     'COD\t75.68\tmg/L\t20230509153700\t00\r\n' +
+//     'Flow\t6.24\tm3/h\t20230509153700\t00\r\n' +
+//     'NH4\t8.21\tmg/L\t20230509153700\t00\r\n' +
+//     'Flow_IN\t298.29\tm3/h\t20230509153700\t00\r\n' +
+//     'Temp\t22.87\toC\t20230509153700\t00\r\n',
+//   'pH\t7.33\t\t20230509153800\t00\r\n' +
+//     'TSS\t6.93\tmg/L\t20230509153800\t00\r\n' +
+//     'COD\t62.98\tmg/L\t20230509153800\t00\r\n' +
+//     'Flow\t40.28\tm3/h\t20230509153800\t00\r\n' +
+//     'NH4\t6.46\tmg/L\t20230509153800\t00\r\n' +
+//     'Flow_IN\t64.98\tm3/h\t20230509153800\t00\r\n' +
+//     'Temp\t23.58\toC\t20230509153800\t00\r\n',
+//   'pH\t7.71\t\t20230509154000\t00\r\n' +
+//     'TSS\t30.92\tmg/L\t20230509154000\t00\r\n' +
+//     'COD\t31.53\tmg/L\t20230509154000\t00\r\n' +
+//     'Flow\t1.10\tm3/h\t20230509154000\t00\r\n' +
+//     'NH4\t1.65\tmg/L\t20230509154000\t00\r\n' +
+//     'Flow_IN\t0.00\tm3/h\t20230509154000\t02\r\n' +
+//     'Temp\t28.93\toC\t20230509154000\t00\r\n'
+// ];
+
+// function splitData(contents){
+//   contents.forEach(element => {
+//     element.split('\t')
+//     console.log(element);
+//   });
+// }
+// splitData(example)
 
 function renderPage(req, res, next) {
   res.render("../views/table.ejs", {
