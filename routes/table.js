2ba484dc5ceacf2d72ecf56664f630a24b94bcb2
TRONG AN_Dam <trongan.dam@vn.panasonic.com>
2023-08-11 14:32:44 +0700

    merge

--- routes/table.js
+++ routes/table.js
@@ -21,86 +21,85 @@ async function getNewestPath(path) {
 async function readNewestFilePath(path, nameDisplay) {
   let parameterOut = {
     nameStation: nameDisplay,
-    params : []
+    params: [],
   };
-  const pathYear = await getNewestPath(path);
-  const pathMonth = await getNewestPath(pathYear);
-  const pathDay = await getNewestPath(pathMonth);
-  const pathFile = await getNewestPath(pathDay); // Path file text
-  const content = await fs.readFile(pathFile, "utf8"); // Data content file txt
-  const spliceData = content.split("\n"); //Bỏ ký tự /n và tách mỗi dòng thành 1 phần tử trong mảng
+  try {
+    const pathYear = await getNewestPath(path);
+    const pathMonth = await getNewestPath(pathYear);
+    const pathDay = await getNewestPath(pathMonth);
+    const pathFile = await getNewestPath(pathDay); // Path file text
+    const content = await fs.readFile(pathFile, "utf8"); // Data content file txt
+    const spliceData = content.split("\n"); //Bỏ ký tự /n và tách mỗi dòng thành 1 phần tử trong mảng
 
-  for (const line of spliceData) {
-    var slicesArray = line.split("\t");
-    //parameterOut[`para${spliceData.indexOf(line)+1}`] = line.split('\t')
-    parameterOut.params.push(
-      {
+    for (const line of spliceData) {
+      var slicesArray = line.split("\t");
+      //parameterOut[`para${spliceData.indexOf(line)+1}`] = line.split('\t')
+      parameterOut.params.push({
         name: slicesArray[0],
         value: slicesArray[1],
         unit: slicesArray[2],
         time: slicesArray[3],
         statuspara: slicesArray[4],
-      }
-    )
+      });
+    }
+    return parameterOut;
+  } catch (error) {
+    alert("Can not read file");
   }
-  return parameterOut;
 }
-
-//readNewestFilePath(pathTable1[0], configs.infor_table_1.name_station_1).then((data) => console.log(data));
-
-// async function sortDataCol(){
-//   const station = configs.infor_table_1;
-//   const valueCol0 = station.name_parameter;
-//   let   valueCol1 = [];
-//   const valueStas1 = await readNewestFilePath(pathTable1[0], station.name_station_1);
-//   const valueStas2 = await readNewestFilePath(pathTable1[1], station.name_station_2);
-//   const valueStas3 = await readNewestFilePath(pathTable1[2], station.name_station_3);
-//   for (let i = 0; i < valueCol0.length; i++){
-    
-//   }
-
-// }
 
 async function renderPage1(req, res, next) {
   const station = configs.infor_table_1;
   const valueCol0 = station.name_parameter;
-  const valueCol1 = await readNewestFilePath(pathTable1[0], station.name_station_1);
-  const valueCol2 = await readNewestFilePath(pathTable1[1], station.name_station_2);
-  const valueCol3 = await readNewestFilePath(pathTable1[2], station.name_station_3);
+  const valueCol1 = await readNewestFilePath(
+    pathTable1[0],
+    station.name_station_1
+  );
+  const valueCol2 = await readNewestFilePath(
+    pathTable1[1],
+    station.name_station_2
+  );
+  const valueCol3 = await readNewestFilePath(
+    pathTable1[2],
+    station.name_station_3
+  );
   res.render("../views/table.ejs", {
     infors: configs,
-    col0 : valueCol0,
-    col1 : valueCol1,
-    col2 : valueCol2,
-    col3 : valueCol3,
+    col0: valueCol0,
+    col1: valueCol1,
+    col2: valueCol2,
+    col3: valueCol3,
     inforTable1: configs.infor_table_1,
   });
-
-
 }
 
 async function renderPage2(req, res, next) {
   const station = configs.infor_table_2;
   const valueCol0 = station.name_parameter;
-  const valueCol1 = await readNewestFilePath(pathTable2[0], station.name_station_1);
-  const valueCol2 = await readNewestFilePath(pathTable2[1], station.name_station_2);
+  const valueCol1 = await readNewestFilePath(
+    pathTable2[0],
+    station.name_station_1
+  );
+  const valueCol2 = await readNewestFilePath(
+    pathTable2[1],
+    station.name_station_2
+  );
   console.log(valueCol0);
   console.log(valueCol1);
   res.render("../views/table2.ejs", {
     infors: configs,
-    col0 : valueCol0,
-    col1 : valueCol1,
-    col2 : valueCol2,
+    col0: valueCol0,
+    col1: valueCol1,
+    col2: valueCol2,
     inforTable1: configs.infor_table_1,
   });
 
-  setTimeout(() =>{
-    res.redirect("/")
-  },3000)
+  setTimeout(() => {
+    res.redirect("/");
+  }, 3000);
 }
 
 router.get("/", renderPage1);
 router.get("/table2", renderPage2);
-
 
 module.exports = router;
